25d0d03475071d0efbb9f6d4fc0f3ed65262aebe
rubenslemos <rubenslemos@gmail.com>
2021-09-13 00:06:10 -0300

    g

--- aulas/aula01/gdrive-webapp/public/app.js
+++ aulas/aula01/gdrive-webapp/public/app.js
@@ -3,7 +3,8 @@ import ConnectionManager from "./src/connectionManager.js"
 import DragAndDropManager from "./src/dragAndDropManager.js"
 import ViewManager from "./src/viewManager.js"
 DragAndDropManager
-const API_URL = "https://localhost:3000"
+//const API_URL = "https://localhost:3000"
+const API_URL = "https://exdrive-webapi.herokuapp.com/"
 
 const appController = new AppController({
     viewManager: new ViewManager(),
